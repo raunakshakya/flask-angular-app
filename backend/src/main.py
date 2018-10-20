773f601e5494288396796c6154ec1caece47119d
Raunak Shakya <rkshakya99@gmail.com>
2018-10-20 19:57:14 +0545

    enable CORS for the flask application

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,12 +1,14 @@
 # coding=utf-8
 
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 from .entities.entity import Session, engine, Base
 from .entities.exam import Exam, ExamSchema
 
 # creating the Flask application
 app = Flask(__name__)
+CORS(app)
 
 # if needed, generate database schema
 Base.metadata.create_all(engine)
