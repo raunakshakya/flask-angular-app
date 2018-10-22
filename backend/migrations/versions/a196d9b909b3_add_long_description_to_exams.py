226fa16ad2ea08a4c8f32f6bb4dff98f56602e8f
Raunak Shakya <rkshakya99@gmail.com>
2018-10-22 20:12:35 +0545

    part 3 - add add_long_description_to_exams migrations

---
+++ backend/migrations/versions/a196d9b909b3_add_long_description_to_exams.py
@@ -0,0 +1,24 @@
+"""add long_description to exams
+
+Revision ID: a196d9b909b3
+Revises: 
+Create Date: 2018-10-22 18:25:51.779043
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a196d9b909b3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
