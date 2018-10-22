6c03af60616157f156f14f5f2928bd3e039e1206
Raunak Shakya <rkshakya99@gmail.com>
2018-10-22 15:44:51 +0545

    part 3 - use Angular Material Cards and CSS Grid layout to show exams

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,7 +12,7 @@ import * as Auth0 from 'auth0-web';
 import {CallbackComponent} from './callback.component';
 
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule, MatButtonModule} from '@angular/material';
+import {MatToolbarModule, MatButtonModule, MatCardModule} from '@angular/material';
 
 const appRoutes: Routes = [
   {path: 'new-exam', component: ExamFormComponent},
@@ -36,6 +36,7 @@ const appRoutes: Routes = [
     NoopAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
+    MatCardModule,
   ],
   providers: [ExamsApiService],
   bootstrap: [AppComponent]
